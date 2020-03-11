103674b6adc81df2d6b621e22f1c389416578a14
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-03-11 21:15:16 +0100

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+    
+        //Números enteros
+        
+        int num = 0;
+        
+        //Números decimales
+        
+        float decimal = 0.1f;
+        
+        //Carácter
+        
+        char a = 'a';
+        
+        //Cadena de caracteres
+        
+        String texto = "cadena";
+        
+        //Verdadero o falso
+        
+        boolean condicional = true;
+}
