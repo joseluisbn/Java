8b74334baffaed14efa583ff9cec7c46d0897e43
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-02 18:03:32 +0100

    Create for.java

---
+++ src/for.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Definición del bucle
+
+        for (int i = 0; i<15; i++)
+        {
+            System.out.println("Ejemplo de bucle for");
+        }
+    }
+
+}
