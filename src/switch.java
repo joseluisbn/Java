998394e98e50ad3320ebe5d311d789cf714d2df9
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-03 00:25:13 +0100

    Create switch.java

---
+++ src/switch.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int opcion;
+
+        opcion = 2;
+
+        switch (opcion){
+
+            case 1:
+                System.out.println("Has elegido la opción 1");
+                break;
+            case 2:
+                System.out.println("Has elegido la opción 2");
+                break;
+            default:
+                System.out.println("No has elegido nada");
+        }
+
+    }
+
+}
