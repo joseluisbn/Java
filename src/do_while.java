04490890c470e5eb78568816bc3869d59e73ea2a
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-02 18:01:33 +0100

    Create do_while.java

---
+++ src/do_while.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        do
+        {
+            System.out.println("Este es un bucle while");
+        } while (a == 6);
+
+            /*Este bucle realiza la acción al menos una vez
+             y se detiene cuando la premisa del while se cumple*/
+    }
+
+}
