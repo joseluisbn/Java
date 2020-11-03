0701f951007a82427ccb619e861105eabf4aef66
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-03 16:01:34 +0100

    Create clasemath.java

---
+++ src/clasemath.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Ejemplos de clase Math
+
+        //Raíz cuadrada
+
+        double num = Math.sqrt(2);
+
+        System.out.println(num);
+
+        //Potencia
+
+        double potencia = Math.pow(5,5);
+        System.out.println(potencia);
+
+        //Redondear
+
+        double redondeo = Math.round(5.56668);
+        System.out.println(redondeo);
+
+        //Constante Pi
+
+        double pi = Math.PI;
+
+        System.out.println(pi);
+    }
+}
+
