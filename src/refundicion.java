3a7e33c3f7803b0de23bd44e7a64680cd9d181ed
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-03 16:06:03 +0100

    Create refundicion.java

---
+++ src/refundicion.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Refundición: cuando queremos cambiar de un tipo de dato a otro
+
+        double num1 = 5.66664;
+
+        int num = (int)Math.round(num1);
+
+        System.out.println(num);
+    }
+}
