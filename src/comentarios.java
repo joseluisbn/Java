e718242875284d2559388760134fd3e09698ecee
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-05 16:21:52 +0100

    Create comentarios.java

---
+++ src/comentarios.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Esto es una línea de comentarios
+
+        /* Esto son varias líneas de comentarios.
+
+        Este tipo de comentarios tienen apertura y cierre.
+
+         */
+
+        System.out.println("Este es el único texto que ejetutará el programa");
+
+    }
+
+}
