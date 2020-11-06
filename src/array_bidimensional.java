a47475205ff9ad83ff0ecd0a5cf386c8a64fffd0
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-06 16:11:12 +0100

    Create array_bidimensional.java

---
+++ src/array_bidimensional.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Definición del array bidimensional
+
+        int [][] matriz = new int[5][5];
+
+        // para recorrer un array bidimensional se utilizan dos bucles for, uno para cada dimensión
+
+        for (int i=0; i<5;i++){
+            System.out.println();
+            for (int j=0; j<5; j++){
+                System.out.print(matriz[i][j]+ " ");
+            }
+        }
+    }
+}
