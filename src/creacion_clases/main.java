b313f87b41c59724afdf403b5f0cd78e37a933ab
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-07 19:25:25 +0100

    Create main.java

---
+++ src/creacion_clases/main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Creamos un nuevo objeto de la clase Computadora
+
+        Computadora computadora1 = new Computadora();
+        System.out.println(computadora1.toString());
+
+    }
+}
